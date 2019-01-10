15112b4016c5cff3db7741c2eaf2da55b2cf2441
Ricardo <ricardocavalcantesilva@gmail.com>
2019-01-09 23:34:35 -0200

    Inclusão das classes

---
+++ ByteBank/Bytebank.Modelos/ContaCorrente.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace Bytebank.Modelos
+{
+    public class ContaCorrente
+    {
+        public static double TaxaOperacao { get; private set; }
+
+        public Cliente Titular { get; set; }
+
+        public int Agencia { get; }
+
+        public int Numero { get; }
+
+        public int ContadorSaquesNaoPermitidos { get; private set; }
+
+        public int ContadorTransferenciasNaoPermitidas { get; private set; }
+
+        private double _saldo = 100;
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+                _saldo = value;
+            }
+        }
+
+        //O método estático é uma característica da Classe e não específica de cada conta
+        public static int TotalDeContasCriadas { get; private set; }
+
+        public ContaCorrente(int agencia, int numero)
+        {
+            if (agencia <= 0)
+            {
+                throw new ArgumentException("O argumento agencia deve ser maior que 0.", nameof(agencia) );
+            }
+
+            if (numero <= 0)
+            {
+                throw new ArgumentException("O argumento numero deve ser maior que 0.", nameof(numero) );
+            }
+
+            Agencia = agencia;
+            Numero = numero;
+
+            TotalDeContasCriadas++;
+
+            TaxaOperacao = 30 / TotalDeContasCriadas;
+        }
+
+        public void Sacar(double valor)
+        {
+            if (valor < 0)
+            {
+                throw new ArgumentException("Valor inválido para o saque", nameof(valor));
+            }
+
+            if (_saldo < valor)
+            {
+                ContadorSaquesNaoPermitidos++;
+                throw new SaldoInsuficienteException(Saldo, valor);    
+            }
+
+            _saldo -= valor;
+            
+        }
+
+        public void Depositar(double valor)
+        {
+            _saldo += valor;
+        }
+
+        public void Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (_saldo < valor)
+            {
+                throw new ArgumentException("Valor inválido para a transferência.", nameof(valor));
+            }
+
+            try
+            {
+                Sacar(valor);
+            }
+            catch (SaldoInsuficienteException ex)
+            {
+                ContadorTransferenciasNaoPermitidas++;
+                throw new OperacaoFinanceiraException("Operação inválida.", ex);
+            }
+
+            contaDestino.Depositar(valor);
+        }
+    }
+}
