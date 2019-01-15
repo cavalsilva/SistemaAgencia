993e68a731672c350fccf5587740160b9c66a8f3
Ricardo Silva <ricardocavalcantesilva@gmail.com>
2019-01-14 22:46:37 -0200

    Revisão de modificadores e protected internal

--- ByteBank/Bytebank.Modelos/Funcionarios/Auxiliar.cs
+++ ByteBank/Bytebank.Modelos/Funcionarios/Auxiliar.cs
@@ -18,7 +18,7 @@ namespace ByteBank.Funcionarios
             Salario *= 1.1;
         }
 
-        public override double GetBonificacao()
+        internal protected override double GetBonificacao()
         {
             return Salario * 0.2;
         }
