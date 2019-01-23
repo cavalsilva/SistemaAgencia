db0668c0c5cc37f5709fa609c629c410527958c8
Ricardo Silva <ricardocavalcantesilva@gmail.com>
2019-01-23 00:04:07 -0200

    lists e expressao regular

--- ByteBank/ByteBank.SistemaAgencia/Program.cs
+++ ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -12,6 +12,44 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
+            var contas = new List<ContaCorrente>()
+            {
+                new ContaCorrente(341, 1),
+                new ContaCorrente(342, 999),
+                null,
+                new ContaCorrente(340, 4),
+                new ContaCorrente(340, 456),
+                new ContaCorrente(340, 10),
+                null,
+                null,
+                new ContaCorrente(290, 123)
+            };
+
+            var contasOrdenadas = contas
+                .Where(conta => conta != null)
+                .OrderBy(conta => conta.Numero);
+
+            foreach (var conta in contasOrdenadas)
+            {
+                Console.WriteLine($"Conta número {conta.Numero}, ag. {conta.Agencia}");
+            }
+
+            Console.ReadLine();
+
+        }
+
+        static int SomarVarios(params int[] numeros)
+        {
+            int acumulador = 0;
+            foreach (int numero in numeros)
+            {
+                acumulador += numero;
+            }
+            return acumulador;
+        }
+
+        static void TestaDataHumanize()
+        {
             // Dia 17 de Agosto de 2018
             DateTime dataFimPagamento = new DateTime(2018, 8, 17);
             // Data corrente no momento de execução do código
@@ -21,6 +59,23 @@ namespace ByteBank.SistemaAgencia
             string mensagem = "Vencimento em " + TimeSpanHumanizeExtensions.Humanize(diferenca);
 
             Console.WriteLine(mensagem);
+            Console.ReadLine();
+        }
+
+        static void TestaArraydeContaCorrente()
+        {
+            ContaCorrente[] contas = new ContaCorrente[]
+            {
+                new ContaCorrente(3032, 346458),
+                new ContaCorrente(3562, 256845),
+                new ContaCorrente(0587, 451258)
+            };
+
+            for(int indice = 0; indice < contas.Length; indice++)
+            {
+                Console.WriteLine($"Conta {indice} {contas[indice].Numero}");
+            }
+
             Console.ReadLine();
         }
     }
